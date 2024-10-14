c913a1e5fc9bb5d70e5564cd7e6a01d02adafaff
alfadiansyah <alfadiansyahyt@gmail.com>
2024-10-14 16:05:08 +0700

    assign to & get folders

--- src/main/java/com/bankmega/ccbmagent/document/controller/GioController.java
+++ src/main/java/com/bankmega/ccbmagent/document/controller/GioController.java
@@ -33,6 +33,13 @@ public class GioController {
         this.gioService = gioService;
     }
 
+    // GET FOLDER
+    @GetMapping("/folders-attachments")
+    public HttpEntity<MampangApiResponse> getFolders() {
+        // Call the service method and return its result
+        return gioService.getFolders();
+    }
+
     // Endpoint for getting assignment data
     @GetMapping("/attachment/data")
     public ResponseEntity<GetAssigntoAttachmentResponse> getAssignmentData(
