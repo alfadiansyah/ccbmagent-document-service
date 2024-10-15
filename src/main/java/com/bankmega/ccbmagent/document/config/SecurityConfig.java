a9d075df3aab629aface88a83fa40a1edbe59adb
johnsungjs <johnsungjs@gmail.com>
2024-10-15 08:01:44 +0700

    edit security, delete npe validation on download document servie

--- src/main/java/com/bankmega/ccbmagent/document/config/SecurityConfig.java
+++ src/main/java/com/bankmega/ccbmagent/document/config/SecurityConfig.java
@@ -3,11 +3,7 @@ package com.bankmega.ccbmagent.document.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.bankmega.ccbmagent.document.services.IpBasedAccessService;
 
@@ -25,6 +21,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/ccbm/document/**").permitAll() // Allow unrestricted access for specific endpoints
+                        //ini di allow dulu yaa selama dev
+                        .requestMatchers("/**").permitAll() // Allow unrestricted access for specific endpoints
                         .anyRequest().authenticated() // Require authentication for all other requests
                 )
                 .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity; enable in production
