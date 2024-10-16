1d791275b6fc2efbac0cb6845b7702aa724c8cfa
johnsungjs <johnsungjs@gmail.com>
2024-10-16 15:06:00 +0700

    add null validation on mapper.getDocumentDownloadCount

--- src/main/java/com/bankmega/ccbmagent/document/model/responses/GetDocumentDownloadCountResponse.java
+++ src/main/java/com/bankmega/ccbmagent/document/model/responses/GetDocumentDownloadCountResponse.java
@@ -1,14 +1,12 @@
 package com.bankmega.ccbmagent.document.model.responses;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetDocumentDownloadCountResponse {
-  private Integer fileDownloadCount;
+  private Integer fileDownloadCount = 0;
 }
